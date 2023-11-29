8f8044205b258e97d590409bf8a4f28ba61bff48
cecile breysse <cecilebreysse.pro@gmail.com>
2023-11-29 10:18:14 +0100

    fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ User.destroy_all
 Team.destroy_all
 Spot.destroy_all
 
-# puts "Creating teams... "
+puts "Creating teams... "
 
 team1 = Team.create!(name: Faker::Team.name)
 team2 = Team.create!(name: Faker::Team.name)
@@ -14,27 +14,27 @@ team3 = Team.create!(name: Faker::Team.name)
 
 teams = [team1, team2, team3]
 
-# puts "Creating users"
+puts "Creating users"
 
-# addresses = ["20 Rue des Capucins, 69001 Lyon", "85 Cours Albert Thomas, 69003, Lyon", "5 Place Saint-Jean, 69005, Lyon", "251 Rue Paul Bert, 69003, Lyon,", "3 Place Bertone, 69004, Lyon", "48 Rue Henon, 69004, Lyon", "11 Place du Marechal Lyautey, 69006, Lyon", "32 Rue de Marseille, 69007, Lyon", "19 Rue Royale, 69001, Lyon", "8 Rue de la Navigation, 69009, Lyon", "21 Quai Victor Augagneur, 69003, Lyon", "33 Avenue Jean Jaures, 69007, Lyon", "11 Boulevard Marius Vivier Merle, 69003, Lyon", "40 Rue de l'Abondance, 69003, Lyon", "16 Quai de Bondy, 69005, Lyon",]
+addresses = ["20 Rue des Capucins, 69001 Lyon", "85 Cours Albert Thomas, 69003, Lyon", "5 Place Saint-Jean, 69005, Lyon", "251 Rue Paul Bert, 69003, Lyon,", "3 Place Bertone, 69004, Lyon", "48 Rue Henon, 69004, Lyon", "11 Place du Marechal Lyautey, 69006, Lyon", "32 Rue de Marseille, 69007, Lyon", "19 Rue Royale, 69001, Lyon", "8 Rue de la Navigation, 69009, Lyon", "21 Quai Victor Augagneur, 69003, Lyon", "33 Avenue Jean Jaures, 69007, Lyon", "11 Boulevard Marius Vivier Merle, 69003, Lyon", "40 Rue de l'Abondance, 69003, Lyon", "16 Quai de Bondy, 69005, Lyon",]
 
-# addresses.each_with_index do |address, i|
-#   counter = i + 1
-#   u = User.create!(
-#     email: "test#{counter}@test.com",
-#     password: "123456",
-#     address: address,
-#     username: Faker::FunnyName.unique.name,
-#     team: teams[counter % teams.size]
-#   )
-#   puts u.email
-# end
+addresses.each_with_index do |address, i|
+  counter = i + 1
+  u = User.create!(
+    email: "test#{counter}@test.com",
+    password: "123456",
+    address: address,
+    username: Faker::FunnyName.unique.name,
+    team: teams[counter % teams.size]
+  )
+  puts u.email
+end
 
-# puts "Users have been created!"
+puts "Users have been created!"
 
-# teams.each { |t| puts "#{t.name} has #{t.users.size}" }
+puts "Users have been assigned to teams!"
 
-# puts "Finished!"
+teams.each { |t| puts "#{t.name} has #{t.users.size}" }
 
 filepath = "db/Cartographie_des_Jardins_de_rue_au_1er_janvier_2022.geojson"
 document = File.read(filepath)
@@ -45,9 +45,12 @@ spots_data.each do |spot_data|
   random_team = teams.sample.id
   p Spot.create!(
     name: spot_data["properties"]["Name"],
+    address: spot_data["properties"]["Arrondissement"],
     latitude: spot_data["properties"]["Lat"],
     longitude: spot_data["properties"]["Lon"],
     spot_type: spot_data["properties"]["Type_de_V__g__talisation"],
     team_id: random_team
   )
 end
+
+puts "Finished!"
