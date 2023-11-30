8af193e010df6486fbb0506cfa4c28c5f404feb5
David Lau <david.kaimun.lau@gmail.com>
2023-11-30 16:19:00 +0100

    redirect to participation show when photo is uploaded

--- app/javascript/controllers/photo_controller.js
+++ app/javascript/controllers/photo_controller.js
@@ -2,10 +2,13 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="photo"
 export default class extends Controller {
+  static targets = ["form"]
+
   connect() {
   }
 
   displayPhoto(event) {
     console.log(event.currentTarget)
+    this.formTarget.submit()
   }
 }
