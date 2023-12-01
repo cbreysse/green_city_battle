ed070e05bc9e36f765175e33a9c9a1b79433ec7c
David Lau <david.kaimun.lau@gmail.com>
2023-12-01 14:06:46 +0100

    calculate every points for each team

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,8 +1,21 @@
 class TeamsController < ApplicationController
   def show
-    @team = Team.find(params[:id])
-    spots = @team.spots
-    participations = Participation.where(spot: spots)
-    @total_points = participations.map { |p| p.action_type.points }.sum
+    @team = current_user.team
+    @my_team_total_points = calculate_total_points(Participation.where(spot: @team.spots))
+
+    all_teams_participations = Participation.joins(:spot).where(spots: { team: Team.all })
+    @total_points_by_team = calculate_total_points_by_team(all_teams_participations)
+  end
+
+  private
+
+  def calculate_total_points(participations)
+    participations.map { |p| p.action_type.points }.sum
+  end
+
+  def calculate_total_points_by_team(participations)
+    participations.group_by { |p| p.spot.team }.transform_values do |team_participations|
+      calculate_total_points(team_participations)
+    end
   end
 end
