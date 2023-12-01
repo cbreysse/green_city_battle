e27815a1ae2299418efd07662b6ab918f0931793
laure Fontanie <fontanie.laure@hotmail.fr>
2023-12-01 12:29:07 +0100

    show team v2

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,7 +1,7 @@
 class TeamsController < ApplicationController
-
   def show
     @team = Team.find(params[:id])
     @teams = Team.all
+    # @teams = Team.all.sort_by { |team| team.score }.reverse ##changer team.score par la fonction implementée par la logique de calcul
   end
 end
