8e81517c3a1dd316ceb987b18ae1f8d41c9a660a
David Lau <david.kaimun.lau@gmail.com>
2023-12-04 17:07:05 +0100

    remove minutes and seconds

--- app/javascript/controllers/timer_controller.js
+++ app/javascript/controllers/timer_controller.js
@@ -33,10 +33,8 @@ export default class extends Controller {
 
       const days = Math.floor(distance / (1000 * 60 * 60 * 24))
       const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
-      const seconds = Math.floor((distance % (1000 * 60)) / 1000)
 
-      this.timeLeftTarget.innerHTML = `${days}j ${hours}h ${minutes}m ${seconds}s`
+      this.timeLeftTarget.innerHTML = `${days}j ${hours}h`
     }, 1000)
   }
 }
