c54c9b6be4da745aadd7920a4e6dda67c6c6c5fc
David Lau <david.kaimun.lau@gmail.com>
2023-12-07 12:01:24 +0100

    move timer from container to badge text

--- app/javascript/controllers/timer_controller.js
+++ app/javascript/controllers/timer_controller.js
@@ -2,8 +2,6 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="timer"
 export default class extends Controller {
-  static targets = [ "timeLeft" ]
-
   static values = {
     spotId: Number,
     actionId: Number,
@@ -18,23 +16,24 @@ export default class extends Controller {
   }
 
   #setCountdown = (data) => {
-    const targetTimestamp = new Date(data.next_possible_action_timestamp
-      ).getTime()
+    if (data.next_possible_action_timestamp) {
+      const targetTimestamp = new Date(data.next_possible_action_timestamp
+        ).getTime()
 
-    const countdownInterval = setInterval(() => {
-      const currentTime = new Date().getTime()
-      const distance = targetTimestamp - currentTime
+      const countdownInterval = setInterval(() => {
+        const currentTime = new Date().getTime()
+        const distance = targetTimestamp - currentTime
 
-      if (distance <= 0) {
-        clearInterval(countdownInterval)
-        this.countdownTarget.textContent = 'Countdown expired'
-        return
-      }
+        if (distance <= 0) {
+          clearInterval(countdownInterval)
+          return
+        }
 
-      const days = Math.floor(distance / (1000 * 60 * 60 * 24))
-      const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24))
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
 
-      this.timeLeftTarget.innerHTML = `${days}j ${hours}h`
-    }, 1000)
+        this.element.innerHTML = ` dans ${days}j ${hours}h`
+      }, 1000)
+    }
   }
 }
